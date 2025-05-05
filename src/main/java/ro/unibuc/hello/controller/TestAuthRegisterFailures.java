af9b3838c23ff1eb01f2680a60f0c6b6a0f4e506
Bogdy555 <64745957+Bogdy555@users.noreply.github.com>
2025-05-05 23:00:52 +0000

    Artificial triggers for monitoring implemented!

---
+++ src/main/java/ro/unibuc/hello/controller/TestAuthRegisterFailures.java
@@ -0,0 +1,42 @@
+package ro.unibuc.hello.controller;
+
+import java.lang.Thread;
+import java.time.Duration;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.micrometer.core.instrument.Counter;
+
+@RestController
+@RequestMapping("/quality")
+public class TestAuthRegisterFailures {
+
+    private Counter registerFailureCounter;
+
+    public TestAuthRegisterFailures(Counter registerFailureCounter) {
+        this.registerFailureCounter = registerFailureCounter;
+    }
+
+    @PostMapping("/down")
+    public ResponseEntity<Void> markDown() {
+        for (int i = 0; i < 1000; i++) {
+            registerFailureCounter.increment();
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/up")
+    public ResponseEntity<Void> markUp() {
+        try
+        {
+            Thread.sleep(5000);
+        }
+        catch (Exception exception)
+        {
+
+        }
+        return ResponseEntity.ok().build();
+    }
+}
